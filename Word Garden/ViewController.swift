38d227711dd4e12a4ab17360056afea3e2c3f7c2
Benjamin Tsai <benjamintsa@gmail.com>
2020-02-01 17:56:56 -0500

    up to lesson 3.4

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         guessLetterButton.isEnabled = false
         playAgainButton.isHidden = true
+        
     }
     
     func updateUIAfterGuess (){
